753853162345af4134dc1d81e93f594d34fd8ddc
ivanpudin <ivanpudin9@gmail.com>
2023-09-08 10:03:10 +0300

    Product markup

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,20 @@
 const Product = (props) => {
-    const { image, title } = props;
+    const { image, title, price } = props;
 
     return (
         <section className="Detail">
             <article className="Detail-thumbnail">
                 <img src={image} alt={title}></img>
+            </article>
+
+            <article className="Detail-info">
+                <div className="Detail-info-header">
+                    <h2>{title}</h2>
+                </div>
+                <div className="Detail-info">
+                    <span className="Detail-info-price">{price}</span>
+                    <span className="Detail-info-rating">Rating: {}</span>
+                </div>
             </article>
         </section>
     );
